dc207cffb37e39aba1bb4f9a94239014331041c8
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-05-31 19:20:39 +0530

    Update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,7 @@
 "use client"
 
-import React from 'react'
-import { authoptions } from '../api/auth/[...nextauth]/route';
-import { signIn, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
+import { signIn } from 'next-auth/react'
+import React, { useState } from 'react'
 
 const page = () => {
     // const { data: session } = useSession(authoptions);
@@ -15,9 +13,27 @@ const page = () => {
     //     redirect('/')
     // }
     
+    const [credentials, setCredentials] = useState({
+        email: ""
+    })
+
+    const handleChange = (e) => {
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        signIn("credentials-login", {...credentials, callbackUrl: `${process.env.NEXT_PUBLIC_HOST}`, redirect: true})
+    }
+
     return (
         <div>
             <h3>Login</h3>
+            {/* <form className='px-4' onSubmit={handleSubmit}>
+                <input type='text' name='email' value={credentials.email} onChange={handleChange} className='p-3 bg-gray-100'/>
+                <button type='submit' className='p-2 bg-green-200 ' >Submit</button>
+            </form> */}
         </div>
     )
 }
