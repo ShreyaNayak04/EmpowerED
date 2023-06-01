4aee9e07561885488b87c8a200a0d5660a19559a
ammshreya <shreyunayak24@gmail.com>
2023-06-01 19:32:31 +0530

    Navbar

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -23,7 +23,7 @@ const page = () => {
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
         <div className="sm:max-w-lg">
            <h1 className='font text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl px-10'>“To any entrepreneur: if you want to do it, do it now. If you don’t, you’re going to regret it.”</h1>
-          <p className='mt-4 text-xl text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsam incidunt beatae ad voluptatem at non, quas iusto eveniet neque quo. Magni facilis exercitationem ipsum. Asperiores hic explicabo pariatur quod.</p>
+          <p className='mt-4 text-small text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsam incidunt beatae ad voluptatem at non, quas iusto eveniet neque quo. Magni facilis exercitationem ipsum. Asperiores hic explicabo pariatur quod.</p>
         </div>
         </div>
         </div>
