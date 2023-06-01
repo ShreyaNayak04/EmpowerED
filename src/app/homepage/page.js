9c7ff89da778cec329c8be4c8b6881bd86fdc5b4
ammshreya <shreyunayak24@gmail.com>
2023-06-01 19:57:53 +0530

    HomePage

--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -2,35 +2,10 @@
 "use client"
 import React from 'react'
 import { signIn } from 'next-auth/react'
-import React, { useState } from 'react'
-const posts = [
 
-
-    {
-      id: 1,
-      title: 'Boost your conversion rate',
-      href: '#',
-      description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      category: { title: 'Marketing', href: '#' },
-      author: {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-    // More posts...
-  ]
 export default function home() {
   return (
-    <div>
       
-    return (
-        // homepage
         <div className="relative mt-1 overflow-hidden bg-white">
 
         <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48 ">
@@ -45,5 +20,6 @@ export default function home() {
 
           </div>
     </div>
+    
   )
 }
